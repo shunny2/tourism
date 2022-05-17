3d910ce2d39d70f7529bcbb36139a2aa7590f155
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-17 16:33:04 -0300

    create command

---
+++ app/Console/Commands/Teste.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class Teste extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:permissions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command performs the creation of permissions.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('permission:create-permission', [
+            'name' => 'admin'
+        ]);
+        $this->call('permission:create-permission', [
+            'name' => 'representative'
+        ]);
+        $this->call('permission:create-permission', [
+            'name' => 'tourist'
+        ]);
+    }
+}
